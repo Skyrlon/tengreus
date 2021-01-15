00a2fc3a7a437161512e938cafd18471a798d4c7
Antoine Drouault <antoinedrouault@hotmail.com>
2021-01-15 23:25:02 +0100

    modified SunIcon design

--- src/components/icons/SunIcon.vue
+++ src/components/icons/SunIcon.vue
@@ -1,14 +1,26 @@
 <template>
   <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-      <radialGradient id="SunGradient">
-        <stop offset="0%" stop-color="yellow"/>
-        <stop offset="40%" stop-color="yellow"/>
-        <stop offset="100%" stop-color="white" stop-opacity="0"/>
-      </radialGradient>
-  </defs>
-
-  <circle cx="50" cy="50" r="49" fill="url(#SunGradient)"/>
+    <defs>
+      <rect
+        id="sun-ray"
+        x="48"
+        y="5"
+        rx="2"
+        r="2"
+        width="4"
+        height="20"
+        fill="#f7f701"
+      />
+    </defs>
+    <use x="0" href="#sun-ray" />
+    <use x="0" href="#sun-ray" transform="rotate(45 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(90 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(135 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(180 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(225 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(270 50 50)" />
+    <use x="0" href="#sun-ray" transform="rotate(315 50 50)" />
+    <circle cx="50" cy="50" r="20" fill="#f7f701" />
   </svg>
 </template>
 
