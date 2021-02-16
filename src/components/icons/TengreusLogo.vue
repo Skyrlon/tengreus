75e4fa792e13e8557f15bdaeb0dcd5c0e8cc3117
Antoine Drouault <antoinedrouault@hotmail.com>
2021-02-16 23:49:06 +0100

    modified Tengreus logo design

--- src/components/icons/TengreusLogo.vue
+++ src/components/icons/TengreusLogo.vue
@@ -7,7 +7,7 @@
   >
     <def>
       <rect
-        id="sun-ray"
+        id="logo_sun-ray"
         x="29"
         y="9"
         rx="2"
@@ -16,38 +16,33 @@
         height="10"
         fill="#f7f701"
       />
+      <linearGradient id="blue-to-grey" x1="0" x2="1" y1="0.40" y2="0.60">
+        <stop offset="25%" stop-color="lightblue" />
+        <stop offset="75%" stop-color="grey" />
+      </linearGradient>
     </def>
-
-    <path d="M120,1 A40,79.5 90,0 0 80,99" fill="lightblue" />
-    <path d="M120,1 A40,79.5 90,0 1 80,99" fill="lightgrey" />
-    <use x="0" href="#sun-ray" />
-    <use x="0" href="#sun-ray" transform="rotate(45 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(90 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(135 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(180 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(225 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(270 30 30)" />
-    <use x="0" href="#sun-ray" transform="rotate(315 30 30)" />
+    <ellipse cx="100" cy="50" rx="99" ry="49" fill="url(#blue-to-grey)" />
+    <use x="0" href="#logo_sun-ray" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(45 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(90 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(135 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(180 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(225 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(270 30 30)" />
+    <use x="0" href="#logo_sun-ray" transform="rotate(315 30 30)" />
     <circle cx="30" cy="30" r="10" fill="#f7f701" />
     <text x="10" y="60" font-size="35" font-weight="bold" font-family="Verdana">
       Tengreus
     </text>
     <path
-      d="M 300,140 
-             a 10,10 0 0,1 0,-25
-             a 10,10 0 0,1 15,-5
-             a 5,5 0 0,1 40,5
-             a 10,10 0 0,1 0,25
-             Z"
+      d="M 300,150 a 10,10 0 0,1 0,-25 a 10,10 0 0,1 15,-5 a 5,5 0 0,1 40,5 a 10,10 0 0,1 0,25 Z"
       fill="grey"
       stroke="black"
       stroke-width="1"
       transform="scale(0.5)"
     />
     <path
-      d="M 320,141
-             l -10,20 10,0 -10,25 25,-27.5 -10,0 10,-17.5
-             Z"
+      d="M 320,151 l -10,20 10,0 -10,25 25,-27.5 -10,0 10,-17.5 Z"
       fill="#f7f701"
       transform="scale(0.5)"
     />
