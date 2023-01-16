c1a2389e8bb7b67cf82a7d7f6f46aa9ca450a038
Antoine Drouault <antoinedrouault@hotmail.com>
2023-01-16 13:43:31 +0100

    Add script to deploy on github pages

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/tengreus/" : "/",
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
-    }
-  }
-}
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true,
+    },
+  },
+};
